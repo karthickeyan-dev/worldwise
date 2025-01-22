a65c166e1ceacca92207d8b807c15f4be92e3a9e
Karthic <jkarthickeyan24@gmail.com>
2025-01-22 16:28:23 +0530

    Add routing and new pages for Homepage, Pricing, and Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+
 export default function App() {
-  return <div>Worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
