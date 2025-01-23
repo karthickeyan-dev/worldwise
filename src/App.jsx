8aa3ce8ce7500ed7ce406ecd179a089941f97c5e
Karthic <jkarthickeyan24@gmail.com>
2025-01-23 10:52:32 +0530

    Add initial components and styles for layout, navigation, and user interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
+import AppLayout from './pages/AppLayout';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
