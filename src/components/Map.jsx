9cae8ac5616bd3a01b294dac8d0bdd366b5fe744
Karthic <jkarthickeyan24@gmail.com>
2025-01-24 20:16:29 +0530

    Add Leaflet and React-Leaflet for map functionality

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,66 @@
-import { useNavigate } from 'react-router-dom';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+} from 'react-leaflet';
+
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
+import { useEffect, useState } from 'react';
+import { useCities } from '../contexts/citiesContext';
 
 export default function Map() {
-  const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  useEffect(() => {
+    if (lat && lng) setMapPosition([lat, lng]);
+  }, [lat, lng]);
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}></div>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {cities.map(city => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangePosition position={mapPosition} />
+        <HandleClick />
+      </MapContainer>
+    </div>
   );
+}
+
+function ChangePosition({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function HandleClick() {
+  const navigate = useNavigate();
+  useMapEvent({
+    click: e => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
