bce4e02c0b5c65ca999c3883b98355d166938ba0
Karthic <jkarthickeyan24@gmail.com>
2025-01-25 19:11:32 +0530

    Add authentication context and integrate user login/logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,31 @@ import CityList from './components/CityList';
 import City from './components/City';
 import CountryList from './components/CountryList';
 import Form from './components/Form';
+import { AuthProvider } from './contexts/AuthContext';
 
 polyfillCountryFlagEmojis();
 
 export default function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import styles from './Login.module.css';
 import PageNav from '../components/PageNav';
+import { useAuth } from '../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
 
 export default function Login() {
-  // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState('jack@example.com');
+  const [email, setEmail] = useState('john@example.com');
   const [password, setPassword] = useState('qwerty');
+
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    if (isAuthenticated) navigate('/app', { replace: true });
+  }, [isAuthenticated, navigate]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
             type="email"
             id="email"
-            onChange={e => setEmail(e.target.value)}
             value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
@@ -26,13 +40,13 @@ export default function Login() {
           <input
             type="password"
             id="password"
-            onChange={e => setPassword(e.target.value)}
             value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
